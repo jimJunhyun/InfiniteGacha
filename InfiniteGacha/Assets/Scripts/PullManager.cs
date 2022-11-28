2925f316525cfcda4c328ed9ca8fbbb965ff406a
jimJunhyun <tsumego2015@gmail.com>
2022-11-29 00:56:47 +0900

    frame application

--- InfiniteGacha/Assets/Scripts/PullManager.cs
+++ InfiniteGacha/Assets/Scripts/PullManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PullManager : MonoBehaviour
 {
     public List<float> rarityRatio = new List<float>(); // 비중. (전체 합) 만큼 뽑았을 때 등장 갯수
 	public int WholePercentage = 100; //전체 확률합. 1000일 경우 정밀도는 0.1까지.
 
+	public UnityEvent OnComp;
+	public List<Sprite> frames;
+
 	List<int> rarityPercentage = new List<int>(); // 전체 100일 경우 확률 (%)
+	List<GachaObject> pulled = new List<GachaObject>();
+	List<Image> frameSlots= new List<Image>();
 	int digit = 0;
 	int rarityRatioSum = 0;
+
+	int idx = 0;
 
 	private void Awake()
 	{
@@ -36,6 +45,8 @@ public class PullManager : MonoBehaviour
 		{
 			rarityPercentage.Add(Mathf.RoundToInt( rarityRatio[i] * WholePercentage / rarityRatioSum));
 		}
+		GetComponentsInChildren( frameSlots);
+		frameSlots.RemoveAt(0);
 	}
 
 	public void StartPull()
@@ -47,7 +58,10 @@ public class PullManager : MonoBehaviour
 			perAdd += rarityPercentage[i];
 			if(per <= perAdd)
 			{
-				Debug.Log("레어도 : "+(i+1));
+				frameSlots[idx].enabled = true;
+				frameSlots[idx].sprite=frames[i];
+				++idx;
+				Debug.Log($"레어도 {i + 1}");
 				break;
 			}
 		}
@@ -71,6 +85,13 @@ public class PullManager : MonoBehaviour
 			yield return null;
 			StartPull();
 		}
-		
+		yield return new WaitUntil(() => { return Input.GetMouseButtonDown(0); });
+		for (int i = 0; i < num; i++)
+		{
+			frameSlots[i].enabled = false;
+		}
+		OnComp.Invoke();
+		idx = 0;
+		pulled = new List<GachaObject>();
 	}
 }
